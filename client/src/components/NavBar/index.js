5854797a4156d8af3334adfb90dd67162b900e2b
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-07 16:52:55 +0100

    improved layout

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {useNavigate as Navigate} from 'react-router-dom';
+import { useNavigate as Navigate } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 
-function NavBar(){
+function NavBar() {
     const goTo = Navigate();
 
-    function goBack(){
+    function goBack() {
         goTo(-1);
     }
 
-    return(<>
-    <NavLink to="" activeClassName="selected" className='link'><h1>Home</h1></NavLink>
-    <NavLink to="settings" activeClassName="selected" className='link'><h1>settings</h1></NavLink>
-    <NavLink to="about" activeClassName="selected" className='link'><h1>about</h1></NavLink>
+    return (<>
+        <NavLink to="" activeClassName="selected" className='link'><h1>Home</h1></NavLink>
+        <NavLink to="settings" activeClassName="selected" className='link'><h1>settings</h1></NavLink>
+        <NavLink to="about" activeClassName="selected" className='link'><h1>about</h1></NavLink>
         <button className='BackBtn' onClick={goBack}>Back</button>
     </>)
 
