f0af474b954e186ac61a9315a569cbf4b36869ad
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-07 14:42:15 +0100

    frontend setup complete

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+    return (<>
+    <h1>App is working!</h1>
+    </>)
+
+
+
+}
+
+export default App;
