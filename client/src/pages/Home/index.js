c3e5ef6676b8ffb7bfe9dc57d970a47ac79005d3
jesse dodoo <jesse.dodoo@gmail.com>
2022-05-09 15:39:37 +0100

    registration page setup

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 //import './styles.css'
 import {BCS} from '../../components'
 import {Login} from '../../components'
+import {Signup} from '../../components'
+import SignupSuccess from '../../components/Signup/SignupSuccess'
 
 
 function Home (){
+
+    const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+
+    const submitForm = () => {
+        setFormIsSubmitted(true)
+    }
     return(<>
     <h1>Home page</h1>
+    { !formIsSubmitted ? <Signup submitForm={submitForm} /> : <SignupSuccess/>}
     <Login/>
     <BCS />
 
