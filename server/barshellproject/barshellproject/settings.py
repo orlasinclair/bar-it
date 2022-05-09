d024af1327d0556c3a31630285904b3e42647e05
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-05-09 14:08:10 +0100

    change settings for deploy

--- server/barshellproject/barshellproject/settings.py
+++ server/barshellproject/barshellproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,6 +84,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -117,11 +122,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
