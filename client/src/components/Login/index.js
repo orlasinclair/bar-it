bba84965d1beef50a4b274a6702ba9e8368d2c30
jesse dodoo <jesse.dodoo@gmail.com>
2022-05-09 11:22:46 +0100

    error handling, login function complete!

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -6,12 +6,29 @@ export default function Login() {
    const [user, setUser] = useState({name:"", email: ""});
    const [error, setError] = useState("")
 
+   const adminUser = {
+       email:"jesse@me.com",
+       password: "admin123"
+   }
+
    const Login = details => {
        console.log(details)
+
+       if (details.email == adminUser.email && details.password == adminUser.password){
+
+           console.log("Logged in");
+           setUser({
+               name: details.name,
+               email: details.email
+           })
+
+       } else {
+           setError("Details do not match!")
+       }
    }
 
    const Logout = () => {
-       console.log("logout")
+        setUser({name: "", email: ""})       
    }
 
   return (
@@ -19,7 +36,7 @@ export default function Login() {
         {(user.email != "") ? (
             <div className='welcome'>
                 <h2>Welcome, <span>{user.name}</span></h2>
-                <button>Logout</button>
+                <button onClick={Logout}>Logout</button>
             </div>
         ): (
             <LoginForm Login={Login} error={error} />
