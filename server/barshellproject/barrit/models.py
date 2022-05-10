f9a2addcf493e80bea286b71b7f1f6fb91b86bc8
jesse dodoo <jesse.dodoo@gmail.com>
2022-05-10 11:04:22 +0100

    login system working python

--- server/barshellproject/barrit/models.py
+++ server/barshellproject/barrit/models.py
@@ -8,6 +8,10 @@ class A_User(models.Model):
     audiodescription = models.BooleanField(default=True)
     darkmode = models.BooleanField(default=False)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length= 50)
+    email = models.CharField(max_length= 50)
+    password = models.CharField(max_length=50)
+
 
 
     def getById(id):
--- server/barshellproject/barrit/urls.py
+++ server/barshellproject/barrit/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('create', views.CreateNewUser, name='create-one'),
     path('<id>', views.GetOne, name='get-one'), 
+    path('signup', views.signup, name="signup" )
 ]
