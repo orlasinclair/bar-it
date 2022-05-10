fe61d57460cea432db55cd1423402fcf19d60f72
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-10 17:25:51 +0100

    ensuring recent changes are saved

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Quagga from 'quagga';
 //import './styles.css'
 //import {BackButton} from '../../components'
 
 
 function About (){
+    Quagga.stop()
     return(<h1 role="heading">about page</h1>)
 
 }
--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Quagga from 'quagga';
 //import './styles.css'
 //import {BackButton} from '../../components'
 
 
 function Settings (){
+    Quagga.stop()
     return(<h1 role="heading">settings page</h1>)
 
 }
