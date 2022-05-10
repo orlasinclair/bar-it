fba3b666d4b93e50c4ac7b54c426ac0c83916c19
jesse dodoo <jesse.dodoo@gmail.com>
2022-05-10 12:14:48 +0100

    spelling error in settings.py

--- server/barshellproject/barshellproject/settings.py
+++ server/barshellproject/barshellproject/settings.py
@@ -138,5 +138,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_UL = "home"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "home" 
