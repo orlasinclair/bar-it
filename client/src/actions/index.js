513e0723fcbd07157aad276fc221c3feaa90794f
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-05-10 16:39:35 +0100

    got settings working and interacting with database

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,8 +4,26 @@ export const getUserSettings = async () => {
     try {
         const response = axios.get('https://barr-it.herokuapp.com/barrit/266f629d-b2fb-4ab3-9eca-7269ed0c3778')
         const data = (await response).data
-        console.log(data)
+        const settingsData = data[0].fields
+        return settingsData
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+export const updateUserSettings = async (userData) => {
+    try {
+        const options = {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+                }
+        }
+        const response = await axios.post('https://barr-it.herokuapp.com/barrit/266f629d-b2fb-4ab3-9eca-7269ed0c3778', userData, options)
+        const data = await response.data
         
+        if (data.err)
+        {throw Error(data.err)}
+        return data
     } catch (error) {
         console.warn(error)
     }
---
+++ client/src/pages/Login/index 2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Login} from '../../components'
+//import './styles.css'
+
+
+function LoginPage (){
+    return(<>
+    <h1 role="heading">Log-in page</h1>
+    <Login />
+    </>)
+
+}
+
+
+export default LoginPage;
