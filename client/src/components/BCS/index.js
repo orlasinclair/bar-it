338c52cf103db2369950c1ff9af211ebb53007e6
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-05-11 14:02:02 +0100

    Update index.js

--- client/src/components/BCS/index.js
+++ client/src/components/BCS/index.js
@@ -71,6 +71,7 @@ function BCS() {
                     width: 480,
                     height: 320,
                     facingMode: "environment",
+                    cameraId: 9
                 },
             },
             decoder: {
