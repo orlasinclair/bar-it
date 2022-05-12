ec66e3523ecfb96ef98e0eb2020149e2ad552dd7
jesse dodoo <jesse.dodoo@gmail.com>
2022-05-12 10:25:21 +0100

    styling hompage complete

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
-//import './styles.css'
+import './style.css'
 import BCS from '../BCS'
 
 
@@ -9,29 +9,29 @@ function Home (){
     const [username, setUsername] = useState('there');
     const [loading, setLoading] = useState(true);
   
-    // useEffect(() => {
-    //     fetch('https://barr-it.herokuapp.com/api/v1/barrit/auth/user/', {
-    //       method: 'GET',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         Authorization: `Token ${localStorage.getItem('token')}`
-    //       }
-    //     })
-    //       .then(res => res.json())
-    //       .then(data => {
-    //         setUsername(data.username);
-    //         setLoading(false);
-    //       });
-    //   }
-    // , []);
+    useEffect(() => {
+        fetch('https://barr-it.herokuapp.com/api/v1/barrit/auth/user/', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${localStorage.getItem('token')}`
+          }
+        })
+          .then(res => res.json())
+          .then(data => {
+            setUsername(data.username);
+            setLoading(false);
+          });
+      }
+    , []);
     
 
     return(<>
-    <h1>Home page</h1>
     <div>
         {loading === false && (
             <Fragment>
                 <h2>Hello {username}!</h2>
+                <p>Happy Scanning! 😊</p>
             </Fragment>
             )}
         </div>
