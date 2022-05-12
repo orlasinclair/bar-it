af020f8215317147478dd6a92afe313b8e2f9158
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-05-12 19:16:33 +0100

    added about page

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSpeechSynthesis } from "react-speech-kit";
 import Quagga from 'quagga';
-//import './styles.css'
+import './style.css'
 //import {BackButton} from '../../components'
 
 
 function About (){
     Quagga.stop()
-    return(<h1 role="heading">about page</h1>)
+    const [about, setAbout] = useState("");
+    const { speak } = useSpeechSynthesis();
+    const msg = new SpeechSynthesisUtterance()
+    msg.text = about
+
+    useEffect(() => {
+        window.speechSynthesis.speak(msg)
+    }, [about])
+
+
+    function hearAbout(){
+        setAbout("Hi there! Welcome to Barr-it!!This an app with the aim of giving more independence and confidence to those who are visually impaired.To use, simply head to the home page scan the barcode of the item you wish to learn more about and wait for the app to work its magic. You should receive back an audio description of the product with loads of useful information!Of course we are an app aimed at making the world a more accessible and inclusive place, and so users can modify their settings. Audio description can be turned off and dark or light mode can be selected based on your preferences. We hope you find this app useful and happy scanning!!")
+        // setAbout("")
+    }
+
+
+    return(<>
+      <p>Hi there! Welcome to Barr-it!!</p>
+      <p>This an app with the aim of giving more independence and confidence to those who are visually impaired.</p>
+      <p>To use, simply head to the home page scan the barcode of the item you wish to learn more about and wait for the app to work its magic. You should receive back an audio description of the product with loads of useful information!</p>
+      <p>Of course we are an app aimed at making the world a more accessible and inclusive place, and so users can modify their settings. Audio description can be turned off and dark or light mode can be selected based on your preferences. </p>
+      <p>We hope you find this app useful and happy scanning!! </p>
+      <button id="aboutBtn" onClick={() => hearAbout()}>Press to hear Audio</button>
+    </>)
 
 }
 
